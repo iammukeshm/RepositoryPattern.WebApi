7d82d3bc11b61b3abb83e97a659a0402ac50bf88
Mukesh Murugan <iammukeshm@gmail.com>
2020-06-28 13:31:42 +0530

    added unit of work

---
+++ DataAccess.EFCore/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess.EFCore.Repositories;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.EFCore.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationContext _context;
+
+        public UnitOfWork(ApplicationContext context)
+        {
+            _context = context;
+            Developers = new DeveloperRepository(_context);
+            Projects = new ProjectRepository(_context);
+        }
+        public IDeveloperRepository Developers { get; private set; }
+
+        public IProjectRepository Projects { get; private set; }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IDeveloperRepository Developers { get; }
+        IProjectRepository Projects { get; }
+        int Complete();
+    }
+}
